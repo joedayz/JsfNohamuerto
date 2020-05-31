9db200957447d9db0b34e9989ab61faa4ccd7fb6
Jose Diaz <jadiaz@farmaciasperuanas.pe>
2020-05-31 18:15:46 -0500

    implementando customers.xhtml

---
+++ webinarjsf/src/main/java/sample/entity/Customer.java
@@ -0,0 +1,94 @@
+package sample.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Past;
+import java.time.LocalDate;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @NotEmpty
+    private String firstName;
+
+    @Column(nullable = false)
+    @NotEmpty
+    private String lastName;
+
+    @Column(nullable = false)
+    @NotEmpty
+    @Email
+    private String email;
+
+    @Past
+    private LocalDate dayOfBirth;
+
+    @Column(nullable = false)
+    private String customerId;
+
+    public Customer() {
+
+    }
+
+    public Customer(String firstName, String lastName, String email, LocalDate dayOfBirth, String customerId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.dayOfBirth = dayOfBirth;
+        this.customerId = customerId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDayOfBirth() {
+        return dayOfBirth;
+    }
+
+    public void setDayOfBirth(LocalDate dayOfBirth) {
+        this.dayOfBirth = dayOfBirth;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+}
---
+++ webinarjsf/src/main/java/sample/web/CustomersBacking.java
@@ -0,0 +1,22 @@
+package sample.web;
+
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@ViewScoped
+public class CustomersBacking implements Serializable {
+    
+    private List<Customer> customers;
+
+    private final Customer customer = new Customer();
+
+    
+}
